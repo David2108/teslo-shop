a0b098a38b823965d9dce3d461724058792520ed
David Vivanco <davidv2108@gmail.com>
2026-06-21 11:44:40 -0500

    Update environment configuration and package dependencies; add product images
    - Added PORT and HOST_API variables to .env.template for server configuration.
    - Updated package.json and package-lock.json to include @nestjs/serve-static and @types/multer for improved static file serving and type definitions.
    - Introduced multiple product images to the public/products directory for enhanced product display.

---
+++ src/files/files.controller.ts
@@ -0,0 +1,57 @@
+import { Controller, Get, Post, Param, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer.helper';
+import type { Response } from 'express';
+import path from 'path';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('files')
+export class FilesController {
+  constructor(
+    private readonly filesService: FilesService,
+  private readonly configService: ConfigService
+  ) { }
+
+  /**
+   * @UseInterceptors(FileInterceptor('file'))
+   * - Activa Multer (gestor de uploads en Express) mediante un interceptor de NestJS.
+   * - Espera una petición multipart/form-data.
+   * - El campo del formulario debe llamarse file (el string 'file' en FileInterceptor('file')).
+   * - Procesa ese archivo antes de entrar al método del controlador.
+   * 
+   * updateFileProduct(@UploadedFile() file: Express.Multer.File)
+   * Es el handler del endpoint:
+   * - @UploadedFile() inyecta el archivo subido en el parámetro file.
+   * - Express.Multer.File es el tipo con metadatos: originalname, mimetype, size,
+   *   buffer (si está en memoria), path (si se guardó en disco), etc.
+   */
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file', {
+    fileFilter: fileFilter,
+    storage: diskStorage({
+      destination: './static/uploads',
+      filename: fileNamer
+    })
+  }))
+  updateFileProduct(@UploadedFile() file: Express.Multer.File) {
+    // console.log({fileInController: file});
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is an image');
+    }
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`
+    return { secureUrl };
+  }
+
+  @Get('product/:imageName')
+  findProduct(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+  ) {
+    console.log(imageName);
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
+}
